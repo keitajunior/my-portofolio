fad298cdad2e9aeef9a1cefdcf68582c108ce86d
keitajunior <keitajunior95@gmail.com>
2025-01-22 14:53:44 +0000

    avoid open new own site

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div id="head">
         <Header sharedData={this.state.sharedData.basic_info} />
         <div className="col-md-12 mx-auto text-center language">
           <div
