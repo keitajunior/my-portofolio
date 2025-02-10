6b1ae6cf17ba28b8a6c8759e10485c6b54dde260
keitajunior <keitajunior95@gmail.com>
2025-02-10 13:33:33 +0000

    add analytics trafic metrics

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 class App extends Component {
   constructor(props) {
     super();
@@ -133,6 +134,8 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+        <SpeedInsights />
+        <Analytics />
       </div>
     );
   }
